314b0935d820a30ffafec12b737e0c05260a1042
MedyOsef <66321746+MedyOsef@users.noreply.github.com>
2020-06-26 17:00:38 +0000

    Delete decipher.py

--- decipher.py
+++
@@ -1,9 +0,0 @@
-entree = input("Chaine a traiter : ")
-cle = input("Cle a utiliser : ")
-sortie, i = "", 0
-for caract in entree:
-    sortie = sortie + chr((ord(caract) + ord(cle[i])) % 256)
-    i = i + 1
-    if i > len(cle) - 1:
-        i = 0
-print(sortie)
